56e0f9567731daed660014317763b59c055fa8b5
Jared Stiff <jstiff@commercehub.com>
2016-12-18 13:17:05 -0800

    Fixed directory problem with bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 TEMP_DIR=`mktemp -d`
+LOG=/tmp/install_gitconfig.log
 
-git clone --depth 1 https://github.com/SeaJaredCode/GitConfig.git $TEMP_DIR
-$TEMP_DIR/install.sh
+set -e
+
+finish() {
+    echo "Removing temporary files."
+    rm -rf $TEMP_DIR >> $LOG
+}
+
+trap finish EXIT
+
+echo "Logging to: $LOG"
+echo "Downloading latest..."
+git clone --progress --depth 1 https://github.com/SeaJaredCode/GitConfig.git $TEMP_DIR 2>> $LOG
+
+echo "Installing..."
+pushd $TEMP_DIR >> $LOG
+./install.sh | tee -a $LOG
+popd >> $LOG
+
+echo "Completed successfully."
