ad2442c2cc930e67fc59bbea215c56add342a75b
Jared Stiff <jstiff@commercehub.com>
2016-12-18 13:05:01 -0800

    Added parameter to install for aliases location

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-cp .gitalias ~/.gitalias
+ALIAS_FILE=${1:-~/.gitalias}
+
+cp .gitalias $ALIAS_FILE
 cp bin/* ~/bin
+
+if [ -z `grep ~/.gitconfig -e "path\s*=.*$(basename $ALIAS_FILE)"` ]
+then
+    echo "It appears that the alias file is not yet included in your gitconfig.
+
+Install by adding the following to ~/.gitconfig:
+
+    [Include]
+        path=$ALIAS_FILE"
+fi
